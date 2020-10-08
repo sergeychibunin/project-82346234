b668e2f5943d4053ac558fc74118f8390ad1fc72
Sergey Chibunin <chibunin1988@gmail.com>
2020-10-08 14:04:57 +0300

    Add the util

---
+++ main.py
@@ -0,0 +1,18 @@
+import asyncio
+import aiohttp
+
+def tasks():
+    async def tsk(i):
+        async with aiohttp.ClientSession() as session:
+            async with session.get('http://httpbin.org/get') as resp:
+                f = open(f'/tmp/file{i}', 'wb')
+                f.write(await resp.read())
+                f.close()
+    return [tsk(i) for i in range(10)]
+
+async def main():
+    await asyncio.gather(*tasks())
+
+
+asyncio.run(main())
+
